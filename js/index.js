4d99d58b3dcb0086962c0d109b7846bd0cb26ae2
Vipin Chaudhary <vipin.chaudhary_cs19@gla.ac.in>
2022-06-04 15:19:22 +0530

    added home page

---
+++ js/index.js
@@ -0,0 +1,6 @@
+  document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#image-carousel', {
+      type: 'loop',
+      autoplay: 'play'
+    } ).mount();
+  } );
---
+++ js/main.js
@@ -0,0 +1,9 @@
+var menu = document.getElementsByClassName('nav-menu');
+
+function openMenu() {
+  menu[0].classList.add('open')
+}
+
+function closeMenu() {
+  menu[0].classList.remove('open')
+}
